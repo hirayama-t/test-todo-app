194d64a1dc6753f8bd36877d8ddaea1259d6c499
hirayama <hirayama@example.com>
2025-12-16 16:27:11 +0900

    テスト修正

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('アプリのタイトルが表示される', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/業務用Todo管理アプリ/);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('説明文が表示される', () => {
+  render(<App />);
+  const descElement = screen.getByText(/タスクの追加・完了・削除ができます/);
+  expect(descElement).toBeInTheDocument();
 });
